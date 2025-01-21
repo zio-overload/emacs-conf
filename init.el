96ab939edcb34e5ac3e4b16a8b7ae61db021f7d7
zzio <emfs2000@gmail.com>
2025-01-20 19:27:38 -0500

    added small modification

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; package -- sumary
 ;;;Commentary:
-;;; This is zio-overload first GNU emacs config
+;;; This is zio-overload first GNU emacs config (test)
 ;;; Code:
 (setq garbage-collection-messages t)
 ;; Set garbage collection threshold to 1GB.
